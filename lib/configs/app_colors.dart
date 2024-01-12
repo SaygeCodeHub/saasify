1a250c26c25cf1776a76318dbdc17f5a689c5bd5
404ajayreddy <arg4644@gmail.com>
2024-01-12 15:37:56 +0530

    Code Cleanup

--- lib/configs/app_colors.dart
+++ lib/configs/app_colors.dart
@@ -5,18 +5,15 @@ class AppColors {
   static const Color black = Color(0xFF14111F);
   static const Color transparent = Color(0x00000000);
   static const Color grey = Color(0xFFF7F9FB);
-  static const Color darkBlue = Color(0xFF38436C);
+
   static const Color lightGrey = Color(0xFFFAFCFE);
 
-  static const Color blue = Color(0xFF38436C);
+  static const Color lightBlue = Color(0xFF345AFA);
+  static const Color darkBlue = Color(0xFF38436C);
   static const Color blueAccent = Color(0xFFD0CCED);
 
   static const Color orange = Color(0xFFFF7043);
 
   static const Color successGreen = Color(0xFF4BB543);
   static const Color errorRed = Color(0xFFE53935);
-  static const Color orange = Color(0xFFFF7043);
-  static const Color grey = Color(0xFFF7F9FB);
-  static const Color transparent = Color(0x00000000);
-  static const Color lightBlue = Color(0xFF345AFA);
 }

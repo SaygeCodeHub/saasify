4f61a9caf26eb884c903a19136307aa1548a63e5
adityarana <adityaranaworks@gmail.com>
2024-01-21 22:15:32 +0530

    SAAS-27: Implement company and branch selection

--- lib/screens/companies/widgets/companies/select_company_button.dart
+++ lib/screens/companies/widgets/companies/select_company_button.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:saasify/caches/cache.dart';
 import 'package:saasify/data/models/authentication/authenticate_user_model.dart';
 import 'package:saasify/di/app_module.dart';
+import 'package:saasify/screens/hrms/hrms_dashboard_screen.dart';
 import '../../../../utils/constants/string_constants.dart';
 import '../../../../widgets/buttons/primary_button.dart';
 import '../branches/all_branches_screen.dart';
@@ -19,8 +20,14 @@ class SelectCompanyButton extends StatelessWidget {
         buttonTitle: StringConstants.kNext,
         onPressed: () {
           getIt<Cache>().setCompanyId(companyName);
-          Navigator.pushNamed(context, AllBranchesScreen.routeName,
-              arguments: [companyName, branches]);
+          if (branches.length <= 1) {
+            getIt<Cache>().setBranchId(branches[0].branchId.toString());
+            Future.delayed(const Duration(seconds: 1));
+            Navigator.pushNamed(context, HRMSDashboardScreen.routeName);
+          } else {
+            Navigator.pushNamed(context, AllBranchesScreen.routeName,
+                arguments: [companyName, branches]);
+          }
         });
   }
 }

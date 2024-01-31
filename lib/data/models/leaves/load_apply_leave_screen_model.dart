44877611c6418ddc4289687634b606c16c69a020
vaishnavichafle <vaishnavic16@gmail.com>
2024-01-31 18:56:29 +0530

    SAAS-48: Implement post api for Apply Leave.

--- lib/data/models/leaves/load_apply_leave_screen_model.dart
+++ lib/data/models/leaves/load_apply_leave_screen_model.dart
@@ -1,15 +1,13 @@
 import 'dart:convert';
 
-LoadApplyLeaveScreenModel loadApplyLeaveScreenModelFromJson(String str) =>
-    LoadApplyLeaveScreenModel.fromJson(json.decode(str));
+LoadApplyLeaveScreenModel loadApplyLeaveScreenModelFromJson(String str) => LoadApplyLeaveScreenModel.fromJson(json.decode(str));
 
-String loadApplyLeaveScreenModelToJson(LoadApplyLeaveScreenModel data) =>
-    json.encode(data.toJson());
+String loadApplyLeaveScreenModelToJson(LoadApplyLeaveScreenModel data) => json.encode(data.toJson());
 
 class LoadApplyLeaveScreenModel {
-  final int status;
-  final String message;
-  final ApplyLeaveData data;
+  int status;
+  String message;
+  ApplyLeaveData data;
 
   LoadApplyLeaveScreenModel({
     required this.status,
@@ -17,24 +15,23 @@ class LoadApplyLeaveScreenModel {
     required this.data,
   });
 
-  factory LoadApplyLeaveScreenModel.fromJson(Map<String, dynamic> json) =>
-      LoadApplyLeaveScreenModel(
-        status: json["status"] ?? '',
-        message: json["message"] ?? '',
-        data: ApplyLeaveData.fromJson(json["data"] ?? ''),
-      );
+  factory LoadApplyLeaveScreenModel.fromJson(Map<String, dynamic> json) => LoadApplyLeaveScreenModel(
+    status: json["status"] ?? "",
+    message: json["message"] ?? "",
+    data: ApplyLeaveData.fromJson(json["data"]),
+  );
 
   Map<String, dynamic> toJson() => {
-        "status": status,
-        "message": message,
-        "data": data.toJson(),
-      };
+    "status": status,
+    "message": message,
+    "data": data.toJson(),
+  };
 }
 
 class ApplyLeaveData {
-  final int casualLeaves;
-  final int medicalLeaves;
-  final List<Approver> approvers;
+  int casualLeaves;
+  int medicalLeaves;
+  List<Approver> approvers;
 
   ApplyLeaveData({
     required this.casualLeaves,
@@ -43,22 +40,21 @@ class ApplyLeaveData {
   });
 
   factory ApplyLeaveData.fromJson(Map<String, dynamic> json) => ApplyLeaveData(
-        casualLeaves: json["casual_leaves"] ?? '',
-        medicalLeaves: json["medical_leaves"] ?? '',
-        approvers: List<Approver>.from(
-            json["approvers"].map((x) => Approver.fromJson(x))),
-      );
+    casualLeaves: json["casual_leaves"] ?? "",
+    medicalLeaves: json["medical_leaves"] ?? "",
+    approvers: List<Approver>.from(json["approvers"].map((x) => Approver.fromJson(x))),
+  );
 
   Map<String, dynamic> toJson() => {
-        "casual_leaves": casualLeaves,
-        "medical_leaves": medicalLeaves,
-        "approvers": List<dynamic>.from(approvers.map((x) => x.toJson())),
-      };
+    "casual_leaves": casualLeaves,
+    "medical_leaves": medicalLeaves,
+    "approvers": List<dynamic>.from(approvers.map((x) => x.toJson())),
+  };
 }
 
 class Approver {
-  final int id;
-  final String approverName;
+  int id;
+  String approverName;
 
   Approver({
     required this.id,
@@ -66,12 +62,12 @@ class Approver {
   });
 
   factory Approver.fromJson(Map<String, dynamic> json) => Approver(
-        id: json["id"],
-        approverName: json["approver_name"],
-      );
+    id: json["id"],
+    approverName: json["approver_name"],
+  );
 
   Map<String, dynamic> toJson() => {
-        "id": id,
-        "approver_name": approverName,
-      };
+    "id": id,
+    "approver_name": approverName,
+  };
 }
--- lib/repositories/leaves/leaves_repository_impl.dart
+++ lib/repositories/leaves/leaves_repository_impl.dart
@@ -6,34 +6,17 @@ import 'package:saasify/repositories/leaves/leaves_repository.dart';
 import 'package:saasify/services/client_services.dart';
 import 'package:saasify/utils/constants/api_constants.dart';
 
-// import 'package:saasify/services/client_services.dart';
-// import 'package:saasify/utils/constants/api_constants.dart';
-
 class LeavesRepositoryImpl implements LeavesRepository {
   final Cache cache = getIt<Cache>();
 
   @override
   Future<LoadApplyLeaveScreenModel> loadApplyLeaveScreen() async {
     try {
-      // var companyId = await cache.getCompanyId();
-      // var branchId = await cache.getBranchId();
-      // var userId = await cache.getUserId();
-      final response = {
-        "status": 200,
-        "message": "Data loaded!",
-        "data": {
-          "casual_leaves": 3,
-          "medical_leaves": 12,
-          "approvers": [
-            {"id": 1, "approver_name": "Vaishnavi"},
-            {"id": 2, "approver_name": "Ajay"},
-            {"id": 3, "approver_name": "Gayatri"},
-            {"id": 4, "approver_name": "Rucha"},
-          ]
-        }
-      };
-      // await ClientServices().get(
-      //     "${ApiConstants.baseUrl}$companyId/$branchId/$userId/${ApiConstants.loadApplyLeaveScreen}");
+      var companyId = await cache.getCompanyId();
+      var branchId = await cache.getBranchId();
+      var userId = await cache.getUserId();
+      final response = await ClientServices().get(
+          "${ApiConstants.baseUrl}$companyId/$branchId/$userId/${ApiConstants.loadApplyLeaveScreen}");
       return LoadApplyLeaveScreenModel.fromJson(response);
     } catch (error) {
       rethrow;
@@ -41,14 +24,14 @@ class LeavesRepositoryImpl implements LeavesRepository {
   }
 
   @override
-  Future<ApplyLeaveModel> applyLeave(Map leaveDetails) async {
-    var companyId = await cache.getCompanyId();
-    var branchId = await cache.getBranchId();
-    var userId = await cache.getUserId();
+  Future<ApplyLeaveModel> applyLeave(Map leaveDetailsMap) async {
     try {
+      var companyId = await cache.getCompanyId();
+      var branchId = await cache.getBranchId();
+      var userId = await cache.getUserId();
       final response = await ClientServices().post(
           "${ApiConstants.baseUrl}$companyId/$branchId/$userId/${ApiConstants.applyLeave}",
-          leaveDetails);
+          leaveDetailsMap);
       return ApplyLeaveModel.fromJson(response);
     } catch (error) {
       rethrow;

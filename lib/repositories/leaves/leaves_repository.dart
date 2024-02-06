f3ae17bc03dc543f12b855b511873b155a5bbc30
vaishnavichafle <48150173+vaishnavichafle@users.noreply.github.com>
2024-02-06 08:48:14 +0530

    SAAS-12: implement put API for update leave status.
    * SAAS-12: Implement put api for Update Leave Status.
    
    * SAAS-12: Implement put api for Update Leave Status.

--- lib/repositories/leaves/leaves_repository.dart
+++ lib/repositories/leaves/leaves_repository.dart
@@ -1,10 +1,14 @@
 import 'package:saasify/data/models/leaves/apply_leave_model.dart';
-import 'package:saasify/data/models/leaves/get_all_leaves.dart';
+import 'package:saasify/data/models/leaves/get_all_leaves_model.dart';
 import 'package:saasify/data/models/leaves/load_apply_leave_screen_model.dart';
+import 'package:saasify/data/models/leaves/update_leave_status_model.dart';
 
 abstract class LeavesRepository {
   Future<LoadApplyLeaveScreenModel> loadApplyLeaveScreen();
 
   Future<ApplyLeaveModel> applyLeave(Map leaveDetailsMap);
+
   Future<GetAllLeavesModel> getAllLeaves();
+
+  Future<UpdateLeaveStatusModel> updateLeaveStatus(Map leaveStatusMap);
 }
---
+++ lib/screens/hrms/leaves/getMyLeaves/my_leaves_mobile_screen.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:saasify/configs/app_spacing.dart';
+import 'package:saasify/data/models/leaves/get_all_leaves_model.dart';
+import 'package:saasify/data/models/table_models/column_data_model.dart';
+import 'package:saasify/widgets/layoutWidgets/background_card_widget.dart';
+import 'package:saasify/widgets/table/custom_table.dart';
+import 'package:saasify/widgets/table/table_cells.dart';
+
+class MyLeavesMobileScreen extends StatelessWidget {
+  final List<MyLeaves> myLeaves;
+
+  const MyLeavesMobileScreen({super.key, required this.myLeaves});
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+        child: BackgroundCardWidget(
+            childScreen: Padding(
+                padding: const EdgeInsets.all(spacingMedium),
+                child: CustomDataTable(
+                    checkboxVisible: false,
+                    showRowCheckBox: false,
+                    columnList: [
+                      ColumnData(header: "Leave Type"),
+                      ColumnData(header: "Start Date"),
+                      ColumnData(header: "End Date"),
+                      ColumnData(header: "Approvers"),
+                      ColumnData(header: "Status"),
+                      ColumnData(header: "Comment")
+                    ],
+                    selectedIds: const [],
+                    dataCount: myLeaves.length,
+                    dataIds:
+                        List.generate(myLeaves.length, (index) => myLeaves),
+                    onRowCheckboxChange: (value) {},
+                    generateData: (index) => [
+                          TableText(text: myLeaves[index].leaveType),
+                          TableText(
+                              text: formatDate(
+                                  myLeaves[index].startDate.toString())),
+                          TableText(
+                              text: formatDate(
+                                  myLeaves[index].endDate.toString())),
+                          TableText(text: myLeaves[index].approvers.join(", ")),
+                          TableText(
+                              text: myLeaves[index].leaveStatus.toString()),
+                          TableText(text: 'Some comment by approver'),
+                        ]))));
+  }
+}
+
+String formatDate(String inputDate) {
+  DateTime dateTime = DateTime.parse(inputDate);
+  String formattedDate = DateFormat('dd/MM/yyyy').format(dateTime);
+  return formattedDate;
+}

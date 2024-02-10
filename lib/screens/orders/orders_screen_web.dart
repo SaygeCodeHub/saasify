b9f85d6a9a2804410b0071fad31a015e88a529d1
adityarana <adityaranaworks@gmail.com>
2024-02-10 22:06:30 +0530

    SAAS-00: Add orders screen

--- lib/screens/orders/orders_screen_web.dart
+++ lib/screens/orders/orders_screen_web.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:saasify/configs/app_colors.dart';
 import 'package:saasify/configs/app_spacing.dart';
 import 'package:saasify/data/models/table_models/column_data_model.dart';
 import 'package:saasify/widgets/layoutWidgets/background_card_widget.dart';
@@ -17,26 +18,39 @@ class OrdersScreenWeb extends StatelessWidget {
             checkboxVisible: false,
             showRowCheckBox: false,
             columnList: [
-              ColumnData(header: "", width: 100),
-              ColumnData(header: "Name"),
-              ColumnData(header: "Employee ID"),
-              ColumnData(header: "Email"),
-              ColumnData(header: "Phone"),
-              ColumnData(header: "Designation"),
+              ColumnData(header: "# Order ID"),
+              ColumnData(header: "Date"),
+              ColumnData(header: "Customer Name"),
+              ColumnData(header: "Contact"),
+              ColumnData(header: "Amount"),
+              ColumnData(header: "Payment Type"),
+              ColumnData(header: "Status"),
             ],
             selectedIds: const [],
             dataCount: 10,
-            dataIds: [0, 1, 2, 3, 4],
+            dataIds: const [0, 1, 2, 3],
             onRowCheckboxChange: (value) {},
             generateData: (index) => [
-                  const TableAvatar(),
-                  TableText(text: 'name'),
-                  TableText(text: 'name'),
-                  TableText(text: 'name'),
-                  TableText(text: 'name'),
-                  TableText(text: 'name'),
+                  TableText(text: '49399123'),
+                  TableText(text: '24/04/2024'),
+                  TableText(text: 'Ramesh Jupiter'),
+                  TableText(text: '+91-8888881890'),
+                  TableText(text: 'Rs. 29,123/-'),
+                  TableText(text: 'Cash'),
+                  TableStatusChips(status: 'PAID', color: statusColor('PAID')),
                 ]),
       ),
     );
+  }
+
+  Color statusColor(String designation) {
+    switch (designation) {
+      case 'PAID':
+        return AppColors.successGreen;
+      case 'PARTIAL':
+        return AppColors.orange;
+      default:
+        return AppColors.errorRed;
+    }
   }
 }

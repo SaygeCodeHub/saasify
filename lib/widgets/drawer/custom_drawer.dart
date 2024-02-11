f4684fc93965dc31c7d537273d2a7f546817893b
adityarana <adityaranaworks@gmail.com>
2024-02-11 22:47:29 +0530

    SAAS-00: Add notification permissions

--- lib/widgets/drawer/custom_drawer.dart
+++ lib/widgets/drawer/custom_drawer.dart
@@ -1,3 +1,94 @@
+// UNCOMMENT THIS CODE TO HAVE ACCESS TO ALL THE MODULES
+// import 'package:flutter/material.dart';
+// import 'package:saasify/configs/app_spacing.dart';
+// import 'package:saasify/screens/hrms/hrms_dashboard_screen.dart';
+// import 'package:saasify/screens/inventory/inventory_screen.dart';
+// import 'package:saasify/screens/orders/orders_screen.dart';
+// import 'package:saasify/widgets/drawer/module_list_tile.dart';
+// import '../../utils/globals.dart';
+// import '../profile/saasify_logo.dart';
+// import '../profile/user_name_widget.dart';
+// import '../profile/user_profile_widget.dart';
+//
+// class CustomDrawer extends StatelessWidget {
+//   const CustomDrawer({super.key});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     bool isMobile = MediaQuery.of(context).size.width < mobileBreakPoint;
+//     return SizedBox(
+//       width: isMobile
+//           ? MediaQuery.sizeOf(context).width * 0.55
+//           : MediaQuery.sizeOf(context).width * 0.15,
+//       height: MediaQuery.sizeOf(context).height,
+//       child: Drawer(
+//         child: ListView(
+//           children: <Widget>[
+//             isMobile
+//                 ? SizedBox(
+//               height: MediaQuery.sizeOf(context).height * 0.1,
+//               child: const Padding(
+//                 padding: EdgeInsets.only(left: spacingXSmall),
+//                 child: Row(
+//                   crossAxisAlignment: CrossAxisAlignment.center,
+//                   children: [
+//                     UserProfileWidget(),
+//                     SizedBox(width: spacingXMedium),
+//                     UserNameWidget()
+//                   ],
+//                 ),
+//               ),
+//             )
+//                 : SizedBox(
+//               height: MediaQuery.sizeOf(context).height * 0.057,
+//               child: Padding(
+//                 padding: EdgeInsets.only(
+//                     left: MediaQuery.sizeOf(context).height * 0.025),
+//                 child: const Align(
+//                   alignment: Alignment.centerLeft,
+//                   child: SaasifyLogo(
+//                     width: 30,
+//                     height: 30,
+//                   ),
+//                 ),
+//               ),
+//             ),
+//             const Divider(),
+//             ModuleListTile(
+//                 iconData: Icons.supervised_user_circle_sharp,
+//                 title: 'HRMS',
+//                 onTap: () {
+//                   Navigator.pushReplacementNamed(
+//                       context, HRMSDashboardScreen.routeName);
+//                 }),
+//             ModuleListTile(
+//                 iconData: Icons.point_of_sale_outlined,
+//                 title: 'POS',
+//                 onTap: () {
+//                   Navigator.pushReplacementNamed(context, 'POS');
+//                 }),
+//             ModuleListTile(
+//                 iconData: Icons.shopping_cart_outlined,
+//                 title: 'Orders',
+//                 onTap: () {
+//                   Navigator.pushReplacementNamed(
+//                       context, OrdersScreen.routeName);
+//                 }),
+//             ModuleListTile(
+//                 iconData: Icons.inventory_outlined,
+//                 title: 'Inventory',
+//                 onTap: () {
+//                   Navigator.pushReplacementNamed(
+//                       context, InventoryScreen.routeName);
+//                 })
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
+//
+
 import 'package:flutter/material.dart';
 import 'package:saasify/caches/cache.dart';
 import 'package:saasify/configs/app_spacing.dart';

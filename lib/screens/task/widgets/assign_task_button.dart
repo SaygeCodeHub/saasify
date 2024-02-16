d59430fb89a842cfa7de59e6ff5b1a202ee5f336
404ajayreddy <arg4644@gmail.com>
2024-02-16 17:21:54 +0530

    SAAS-TaskManagement : Implemented Assign Task Api

--- lib/screens/task/widgets/assign_task_button.dart
+++ lib/screens/task/widgets/assign_task_button.dart
@@ -11,9 +11,13 @@ import 'package:saasify/widgets/alertDialogs/success_alert_dialog.dart';
 import 'package:saasify/widgets/buttons/primary_button.dart';
 
 class AssignTaskButton extends StatelessWidget {
+  final double buttonWidth;
   final GlobalKey<FormState> formKey;
 
-  const AssignTaskButton({super.key, required this.formKey});
+  const AssignTaskButton(
+      {super.key,
+      required this.formKey,
+      this.buttonWidth = kGeneralActionButtonWidth});
 
   @override
   Widget build(BuildContext context) {
@@ -47,7 +51,7 @@ class AssignTaskButton extends StatelessWidget {
                   child: Center(child: CircularProgressIndicator()));
             }
             return PrimaryButton(
-              buttonWidth: kGeneralActionButtonWidth,
+              buttonWidth: buttonWidth,
               onPressed: () {
                 if (formKey.currentState?.validate() ?? false) {
                   context.read<TaskBloc>().add(AssignTask());

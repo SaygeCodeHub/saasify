06ab7df143b780c53b38f39a31bc9bfdf9ee9b9e
vaishnavichafle <vaishnavic16@gmail.com>
2024-03-10 01:06:05 +0530

    SAAS-pdf: barcode generator.

--- lib/utils/pdf/pdfFormats/retail_bill_gst_percent_pdf.dart
+++ lib/utils/pdf/pdfFormats/retail_bill_gst_percent_pdf.dart
@@ -1,5 +1,4 @@
 import 'dart:html' as html;
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';

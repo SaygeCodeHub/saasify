cc83379bfec68bd8835babc48f2b787a79e46a58
adityarana <adityaranaworks@gmail.com>
2024-03-26 11:14:02 +0530

    SAAS-00: Offline First

--- lib/screens/companyDetails/register_company_screen.dart
+++ lib/screens/companyDetails/register_company_screen.dart
@@ -1,38 +1,32 @@
 import 'dart:typed_data';
-
 import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
 import '../../../configs/app_spacing.dart';
 import '../../models/user/user_model.dart';
 import '../skeleton_screen.dart';
 import '../widgets/buttons/primary_button.dart';
+import '../widgets/form_widgets.dart';
 import '../widgets/image_picker_widget.dart';
-import '../widgets/lable_and_textfield_widget.dart';
 
 class RegisterCompanyWebScreen extends StatefulWidget {
   const RegisterCompanyWebScreen({super.key});
 
   @override
-  State<RegisterCompanyWebScreen> createState() =>
-      _RegisterCompanyWebScreenState();
+  RegisterCompanyWebScreenState createState() =>
+      RegisterCompanyWebScreenState();
 }
 
-class _RegisterCompanyWebScreenState extends State<RegisterCompanyWebScreen> {
-  final TextEditingController textEditingControllerOwnerName =
+class RegisterCompanyWebScreenState extends State<RegisterCompanyWebScreen> {
+  final formKey = GlobalKey<FormState>();
+  final TextEditingController ownerNameController = TextEditingController();
+  final TextEditingController companyNameController = TextEditingController();
+  final TextEditingController identificationNumberController =
       TextEditingController();
-  final TextEditingController textEditingControllerCompanyName =
-      TextEditingController();
-  final TextEditingController textEditingControllerIdentificationNumber =
-      TextEditingController();
-  final TextEditingController textEditingControllerAddress =
-      TextEditingController();
-
+  final TextEditingController addressController = TextEditingController();
   Uint8List? _imageBytes;
 
   void _handleImagePicked(Uint8List imageBytes) {
-    setState(() {
-      _imageBytes = imageBytes;
-    });
+    setState(() => _imageBytes = imageBytes);
   }
 
   @override
@@ -40,88 +34,69 @@ class _RegisterCompanyWebScreenState extends State<RegisterCompanyWebScreen> {
     return SkeletonScreen(
       appBarTitle: 'Add Company',
       bodyContent: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            ImagePickerWidget(
-              label: 'Company Logo',
-              initialImage: _imageBytes,
-              onImagePicked: _handleImagePicked,
-            ),
-            const SizedBox(height: spacingHuge),
-            LabelAndTextFieldWidget(
-              prefixIcon: const Icon(Icons.person),
-              label: 'Owner Name',
-              isRequired: true,
-              textFieldController: textEditingControllerOwnerName,
-            ),
-            const SizedBox(height: spacingStandard),
-            LabelAndTextFieldWidget(
-              prefixIcon: const Icon(Icons.business),
-              label: 'Company Name',
-              isRequired: true,
-              textFieldController: textEditingControllerCompanyName,
-            ),
-            const SizedBox(height: spacingStandard),
-            LabelAndTextFieldWidget(
-              prefixIcon: const Icon(Icons.numbers_outlined),
-              label: 'EIN / TIN / GST Number',
-              isRequired: true,
-              textFieldController: textEditingControllerIdentificationNumber,
-            ),
-            LabelAndTextFieldWidget(
-              prefixIcon: const Icon(Icons.location_city),
-              label: 'Address',
-              maxLines: 2,
-              isRequired: false,
-              textFieldController: textEditingControllerAddress,
-            ),
-          ],
+        child: Form(
+          key: formKey,
+          child: _buildFormBody(),
         ),
       ),
-      bottomBarButtons: [
-        PrimaryButton(
-          buttonTitle: 'Save Profile Details',
-          onPressed: () async {
-            saveUserDetailsToLocalDatabase(
-                context,
-                _imageBytes,
-                textEditingControllerOwnerName,
-                textEditingControllerCompanyName,
-                textEditingControllerIdentificationNumber,
-                textEditingControllerAddress);
-          },
-        ),
+      bottomBarButtons: _buildBottomBarButtons(context),
+    );
+  }
+
+  Widget _buildFormBody() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: <Widget>[
+        ImagePickerWidget(
+            label: 'Company Logo',
+            initialImage: _imageBytes,
+            onImagePicked: _handleImagePicked),
+        const SizedBox(height: spacingHuge),
+        buildTextField(ownerNameController, 'Owner Name', Icons.person, true),
+        buildTextField(
+            companyNameController, 'Company Name', Icons.business, true),
+        buildTextField(identificationNumberController, 'EIN / TIN / GST Number',
+            Icons.numbers_outlined, true),
+        buildTextField(addressController, 'Address', Icons.location_city, false,
+            maxLines: 2),
       ],
     );
   }
-}
 
-Future<void> saveUserDetailsToLocalDatabase(context, imageBytes, ownerName,
-    companyName, identificationNumber, address) async {
-  if (ownerName.text.isEmpty ||
-      companyName.text.isEmpty ||
-      identificationNumber.text.isEmpty) {
-    ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-      content: Text('Please fill in all required fields.'),
-    ));
-    return;
+  List<Widget> _buildBottomBarButtons(BuildContext context) {
+    return [
+      PrimaryButton(
+        buttonTitle: 'Save Profile Details',
+        onPressed: () async {
+          if (formKey.currentState!.validate()) {
+            await saveUserDetailsToLocalDatabase();
+          }
+        },
+      ),
+    ];
   }
 
-  final newCompany = UserModel(
-    ownerName: ownerName.text,
-    companyName: companyName.text,
-    identificationNumber: identificationNumber.text,
-    logo: imageBytes,
-    address: address.text.isEmpty ? null : address.text,
-  );
+  Future<void> saveUserDetailsToLocalDatabase() async {
+    final newCompany = UserModel(
+      ownerName: ownerNameController.text,
+      companyName: companyNameController.text,
+      identificationNumber: identificationNumberController.text,
+      logo: _imageBytes,
+      address:
+          addressController.text.isNotEmpty ? addressController.text : null,
+    );
 
-  final companiesBox = Hive.box<UserModel>('userDetails');
-  await companiesBox.add(newCompany);
-
-  ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-    content: Text('Company added successfully'),
-  ));
-  await companiesBox.close();
+    try {
+      final companiesBox = Hive.box<UserModel>('userDetails');
+      await companiesBox.add(newCompany);
+      if (!mounted) return;
+      ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text('Company added successfully')));
+    } catch (e) {
+      if (!mounted) return;
+      ScaffoldMessenger.of(context)
+          .showSnackBar(SnackBar(content: Text('Failed to add company: $e')));
+    }
+  }
 }
